7d0716ce43b9b751abebd9ad9b3e2ee186ae5b84
vlachcj <vlachcj@umich.edu>
2014-11-19 13:36:14 -0500

    moved global pin names into this header

--- textilesensor.h
+++ textilesensor.h
@@ -4,6 +4,19 @@
 #include "target.h"
 #include "lightcombo.h"
 
+
+  int ledPinB1 = 3;                                 //Blue LED
+  int ledPinB2 = 4;                                 //Blue LED
+  int ledPinB3 = 5;                                 //Blue LED
+
+  int ledPinR1 = 6;                                //Red LED
+  int ledPinR2 = 7;                                //Red LED
+  int ledPinR3 = 8;                                //Red LED
+
+  int ledPinY1 = 9;                                //Yellow LED
+  int ledPinY2 = 10;                                //Yellow LED
+
+
 class TextileSensor { // this class should be friends with LEDController
 
 friend class DJ;

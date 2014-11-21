3a37b1e575b0fe23bd47d7536f51166439deba8e
vlachcj <vlachcj@umich.edu>
2014-11-21 13:41:02 -0500

    constructor clean-up

--- textilesensor.cpp
+++ textilesensor.cpp
@@ -4,7 +4,6 @@
 TextileSensor::TextileSensor() {
 	// Fill this in once we know the pin numbers for each target
   // each target is associated with an output LED pin and an input capacitive sensor pin. 
-<<<<<<< HEAD
 	targets[0] = Target( ledPinB1, analogPinB1, capSensPinB1_1, capSensPinB1_2);
  	targets[1] = Target( ledPinB2, analogPinB2, capSensPinB2_1, capSensPinB2_2);
   	targets[2] = Target( ledPinB3, analogPinB3, capSensPinB3_1, capSensPinB3_2);
@@ -13,16 +12,6 @@ TextileSensor::TextileSensor() {
   	targets[5] = Target( ledPinR3, analogPinR3, capSensPinR3_1, capSensPinR3_2);
   	targets[6] = Target( ledPinY1, analogPinY1, capSensPinY1_1, capSensPinY1_2);
   	targets[7] = Target( ledPinY2, analogPinY2, capSensPinY2_1, capSensPinY2_2);
-=======
-	  targets[0] = Target( ledPinB1, touchPinB1);
- 	  targets[1] = Target( ledPinB2, touchPinB2);
-  	targets[2] = Target( ledPinB3, touchPinB3);
- 	  targets[3] = Target( ledPinR1, touchPinR1);
-  	targets[4] = Target( ledPinR2, touchPinR2);
-  	targets[5] = Target( ledPinR3, touchPinR3);
-  	targets[6] = Target( ledPinY1, touchPinY1);
-  	targets[7] = Target( ledPinY2, touchPinY2);
->>>>>>> 71381feb480f073d859b43eee00ad1ea85b8849a
 			
 } 
 
